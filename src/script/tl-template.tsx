53cfe96ee055b4ad749e9d67f31c2749516bf03c
Alex Murray <alex.murray@iress.com>
2021-11-02 01:25:54 +1100

    Lots of cleanup. Enzyme is back.

---
+++ src/script/tl-template.tsx
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom";
+// import { render, fireEvent, getByText } from "@testing-library/react";
+// import React from "react";
+
+describe("Code under test", () => {
+  it("fulfils requirement", () => {});
+});
--- src/script/ts-test.test.ts
+++
@@ -1,7 +0,0 @@
-import { testableFunction } from "./ts-testable";
-
-describe("testable function", () => {
-  it("returns 'result' as a string", () => {
-    expect(testableFunction()).toBe("Result");
-  });
-});
