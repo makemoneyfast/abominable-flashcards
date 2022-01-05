ec0aa520a1ec53a11afb6a0da0c65e7433f531ca
Alex Murray <make.money.fast@gmail.com>
2022-01-05 23:04:52 +0900

    Better styling for tags and sets.

--- src/script/SetManager.tsx
+++ src/script/SetManager.tsx
@@ -10,61 +10,59 @@ import _ from "Lodash";
 import "./styles/setManager.less";
 
 interface SetManagerProps {
-    sets: { name: string; cardCount: number; id: string }[];
-    onEditSet: (setID: string) => void;
-    onDeleteSet: (setID: string) => void;
+  sets: { name: string; cardCount: number; id: string }[];
+  onEditSet: (setID: string) => void;
+  onDeleteSet: (setID: string) => void;
 }
 
 const BasicSetManager: React.FunctionComponent<SetManagerProps> = (
-    props: SetManagerProps
+  props: SetManagerProps
 ) => {
-    const sets = _(props.sets)
-        .map(set => (
-            <div key={set.id} className="set">
-                {set.name} <strong>{set.cardCount}</strong>
-                <input
-                    type="button"
-                    value="edit"
-                    onClick={() => props.onEditSet(set.id)}
-                />
-                <input
-                    type="button"
-                    value="delete"
-                    onClick={() => props.onDeleteSet(set.id)}
-                />
-            </div>
-        ))
-        .value();
-    return <div className="setManager">{sets}</div>;
+  const sets = _(props.sets)
+    .map((set) => (
+      <div key={set.id} className="set">
+        {set.name} <strong>{set.cardCount}</strong>{" "}
+        <input
+          type="button"
+          value="編集"
+          onClick={() => props.onEditSet(set.id)}
+        />
+        <input
+          type="button"
+          value="削除"
+          onClick={() => props.onDeleteSet(set.id)}
+        />
+      </div>
+    ))
+    .value();
+  return <div className="setManager">{sets}</div>;
 };
 
-const mapStateToProps: (state: State) => SetManagerProps = (
-    state: State
-) => {
-    const sets = _(state.assets.sets)
-        .map(set => ({
-            name: set.name,
-            cardCount: set.kanji.length,
-            id: set.id
-        }))
-        .value();
-    return { sets } as SetManagerProps;
+const mapStateToProps: (state: State) => SetManagerProps = (state: State) => {
+  const sets = _(state.assets.sets)
+    .map((set) => ({
+      name: set.name,
+      cardCount: set.kanji.length,
+      id: set.id,
+    }))
+    .value();
+  return { sets } as SetManagerProps;
 };
 
-const mapDispatchToProps: (
-    dispatch: Dispatch<Action>
-) => SetManagerProps = (dispatch: Dispatch<Action>) => {
-    return {
-        onEditSet: (setID: string) =>
-            dispatch(thunkEditNominatedSet(setID, "set_manager") as any),
-        onDeleteSet: (setID: string) =>
-            dispatch(thunkDeleteSetAndFlush(setID) as any)
-    } as SetManagerProps
+const mapDispatchToProps: (dispatch: Dispatch<Action>) => SetManagerProps = (
+  dispatch: Dispatch<Action>
+) => {
+  return {
+    onEditSet: (setID: string) =>
+      dispatch(thunkEditNominatedSet(setID, "set_manager") as any),
+    onDeleteSet: (setID: string) =>
+      dispatch(thunkDeleteSetAndFlush(setID) as any),
+  } as SetManagerProps;
 };
 
 const SetManager = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(BasicSetManager);
 
 export default SetManager;
